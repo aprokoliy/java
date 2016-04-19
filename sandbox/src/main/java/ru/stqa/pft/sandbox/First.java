17660c149655f39968147e1917cb7af6f80db5a5
aprokoliy <ann.prokoliy@gmail.com>
2016-04-19 22:20:09 +0400

    Package was created

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class First{
+
+public static void main(String[] args){
+  System.out.println("Hello, world!");
+  System.out.println("площадь квадрата со стороной" +5+ "="+ (5*5));
+}
+
+
+}
