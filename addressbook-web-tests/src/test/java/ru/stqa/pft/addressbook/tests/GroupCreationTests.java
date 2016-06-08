74de9fbe719bfd9bc60e61b81de091a45eadaebd
aprokoliy <ann.prokoliy@gmail.com>
2016-06-08 19:29:33 +0300

    Lambda

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,9 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +16,12 @@ public class GroupCreationTests extends TestBase {
 
         app.goTo().groupPage();
         List<GroupData> before = app.group().getGroupList();
-        GroupData group = new GroupData().withName("test4");
+        GroupData group = new GroupData().withName("test45");
         app.group().create(group);
         List<GroupData> after = app.group().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-
-        int max = 0;
-        for(GroupData g: after){
-            if(g.getId()>max){
-                max=g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
         before.add(group);
        // Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
