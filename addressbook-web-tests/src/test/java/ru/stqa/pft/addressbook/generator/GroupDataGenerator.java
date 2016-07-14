6c28a2692a9a2e1c87bb0c9a9c61609150267c44
aprokoliy <ann.prokoliy@gmail.com>
2016-07-14 15:29:27 +0300

    Xml+json+Jcommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
@@ -1,8 +1,13 @@
 package ru.stqa.pft.addressbook.generator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import javax.swing.plaf.metal.MetalIconFactory;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,14 +19,58 @@ import java.util.List;
  * Created by user on 02-Jul-16.
  */
 public class GroupDataGenerator {
+    @Parameter(names = "-c",description = "Group count")
+    public int count;
+
+    @Parameter(names= "-f", description = "Target file")
+    public String file;
+
+    @Parameter(names= "-d", description = "Data Format")
+    public String format;
+
     public static void main (String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-        List<GroupData> groups  = generateGroups(count);
-        save(groups, file);
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander commander = new JCommander(generator);
+        try {
+            commander.parse(args);
+        } catch(ParameterException ex) {
+            commander.usage();
+            return;
+        }
+        generator.run();
+    }
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        if (format.equals("csv")) {
+            saveAsCsv(groups, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXml(groups, new File(file));
+        } else if (format.equals("json")) {
+            saveAsJson(groups, new File(file));
+        }else {
+            System.out.println("Unrecognized format " + format);
+        }
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json  = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+
+    }
+
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private  void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for(GroupData group: groups){
             writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
@@ -29,7 +78,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private  List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for(int i=0; i<count;i++){
          groups. add(new GroupData().withName(String.format("test %s", i)).
