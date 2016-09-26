ea518ea3b04110bdb305222a42dc2a68db94bb7a
aprokoliy <ann.prokoliy@gmail.com>
2016-09-26 10:32:42 +0300

    @SuppressWarnings("unchecked")

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactcreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactcreation.java
@@ -6,7 +6,7 @@ import ru.stqa.pft.addressbook.model.ContactAddressData;
 import ru.stqa.pft.addressbook.model.ContactPersonalData;
 
 import java.io.File;
-
+@SuppressWarnings("unchecked")
 public class NewContactCreation extends TestBase {
 
     @Test
